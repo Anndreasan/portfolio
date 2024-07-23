b27d8f1634bf83b645568ac89cc7756773f1d177
Anndreasan <122876462+Anndreasan@users.noreply.github.com>
2024-07-23 13:42:07 +0200

    diploma pop up

--- js/theme.js
+++ js/theme.js
@@ -375,3 +375,16 @@ form.on('submit', function (e) {
 });
 
 })(jQuery)
+
+function openModal(imageSrc) {
+	var modal = document.getElementById("myModal");
+	var modalImage = document.getElementById("modalImage");
+	modal.style.display = "block";
+	modalImage.src = imageSrc;
+  }
+  
+  function closeModal() {
+	var modal = document.getElementById("myModal");
+	modal.style.display = "none";
+  }
+  
